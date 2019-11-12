313d9e3a67e845dfbf08791d90373fad56e87919
Ben Tagtow <bentagtow@gmail.com>
2019-11-12 23:09:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -60,6 +60,9 @@ end
 
 
 def gross_per_studio(collection)
+  index = 0 
+  result = {}
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
